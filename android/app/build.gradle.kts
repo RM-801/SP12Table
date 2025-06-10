5c0aca2336e2b0b7b30690b010b68061ba8e6b6f
SeaRay <andylaw5@live.com>
2025-06-11 05:58:53 +0800

    退回两个有问题的文件

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "hk.sirocco.sp12table"
+    namespace = "com.example.sp12table"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "hk.sirocco.sp12table"
+        applicationId = "com.example.sp12table"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
